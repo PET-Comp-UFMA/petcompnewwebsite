8e85f41e11f9cf26e418de6d22236fe5dbaeb71b
Yan Felipe <yanfelipebp@gmail.com>
2026-02-23 12:07:39 -0300

    Desenvolvendo infos dos banners

--- banners.php
+++ banners.php
@@ -28,77 +28,108 @@
                 Descubra como o grupo tem contribuído para a formação de estudantes, o fortalecimento da comunidade acadêmica e o avanço do conhecimento científico na área da Computação.</p>
             </div>
 
-            <div class="carousel-section">
-                <div class="carousel-container">
+            <section>
+                <div class="carousel-section">
+                    <div class="carousel-container">
 
-                    <div class="swiper carousel">
-                        <div class="swiper-wrapper">
-                        
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/01.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/02.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/03.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/04.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/06.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/07.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/08.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/09.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/10.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/12.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/13.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/14.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/15.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/17.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/18.jpg" alt="">
-                            </div>
-                            <div class="swiper-slide">
-                                <img src="./assets/banners/19.jpg" alt="">
+                        <div class="swiper carousel">
+                            <div class="swiper-wrapper">
+                            
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/01.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/02.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/03.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/04.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/06.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/07.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/08.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/09.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/10.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/12.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/13.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/14.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/15.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/17.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/18.jpg" alt="">
+                                </div>
+                                <div class="swiper-slide">
+                                    <img src="./assets/banners/19.jpg" alt="">
+                                </div>
+
                             </div>
 
+                            <div class="swiper-controls">
+                                <div class="swiper-button-prev" aria-label="Banner anterior"></div>
+                                <div class="swiper-pagination"></div>
+                            <div class="swiper-button-next" aria-label="Próximo banner"></div>
                         </div>
-
-                        <div class="swiper-controls">
-                            <div class="swiper-button-prev" aria-label="Banner anterior"></div>
-                            <div class="swiper-pagination"></div>
-                        <div class="swiper-button-next" aria-label="Próximo banner"></div>
+                        </div>
                     </div>
-                    </div>
-
                 </div>
-            </div>
+            </section>
 
             <div class="banner-modal" id="bannerModal">
                 <span class="banner-modal-close" id="closeModal">&times;</span>
                 <img class="banner-modal-img" id="modalImage" alt="Banner ampliado">
             </div>
+
+            <section class="banner-info-section">
+                <div class="info-container">
+                    <div class="swiper info-carousel">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-info">
+                                <div class="banner-card">
+                                    <h3>Título do Banner</h3>
+
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.  Asperiores eius est neque, enim debitis, voluptas voluptatum 
+                                    autem aspernatur totam quia facere? Doloremque aliquid alias cupiditate deserunt distinctio nesciunt necessitatibus ratione.</p>
+
+                                    <div class="banner-card-actions">
+                                        <button  class="btn-view" data-img="./assets/banners/01.jpg">
+                                            Visualizar
+                                        </button>
+                                        
+                                        <a href="./assets/banners/01.jpg" download class="btn-download">
+                                            Download
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
 
         <?php include('footer.php') ?>
         <script src="./js/js.js"></script>
