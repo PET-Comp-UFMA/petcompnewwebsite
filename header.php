adf507b4e5b3d5e468604e005489aadb45e60b78
José Guilherme Magalhães de Almeida <oficialjoseguilherme@gmail.com>
2026-02-27 13:33:49 -0300

    FIX header.php: convertendo trigger links dos dropdowns para botões

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
             <a href="" class="nodropbtn">Início</a>
         </div>
         <div class="nav-item dropdown">
-            <a href="#" class="dropbtn">Conheça o PETComp <span class="material-icons">arrow_drop_down</span></a>
+            <button class="dropbtn">Conheça o PETComp <span class="material-icons">arrow_drop_down</span></button>
             <div class="dropdown-content">
                 <a href="sobre">Sobre nós</a>
                 <a href="integrantes">Integrantes</a>
             </div>
         </div>
         <div class="nav-item dropdown dropdown_projetos">
-            <a href="#" class="dropbtn">Projetos <span class="material-icons">arrow_drop_down</span></a>
+            <button class="dropbtn">Projetos <span class="material-icons">arrow_drop_down</span></button>
             <div class="dropdown-content">
                 <a href="monitoria">Monitoria</a>
                 <a href="desenvolvimento">Fáb. de software </a>
@@ -20,7 +20,7 @@
             </div>
         </div>
         <div class="nav-item dropdown">
-            <a href="#" class="dropbtn">Publicações <span class="material-icons">arrow_drop_down</span></a>
+            <button class="dropbtn">Publicações <span class="material-icons">arrow_drop_down</span></button>
             <div class="dropdown-content">
                 <a href="publicacoes">Trabalhos Publicados</a>
                 <a href="revista">Revista PETComp</a>
@@ -31,7 +31,7 @@
             <a href="noticias">Notícias</a>
         </div>
         <div class="nav-item dropdown dropdown_produtos">
-            <a href="#" class="dropbtn">Produtos <span class="material-icons">arrow_drop_down</span></a>
+            <button class="dropbtn">Produtos <span class="material-icons">arrow_drop_down</span></button>
             <div class="dropdown-content">
                 <a href="biblioteca">Biblioteca PETComp</a>
                 <a href="podcast">PodCast</a>
