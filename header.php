9fe7c70ca1ff93a0bec243d30df99dfbf0c5af67
José Guilherme Magalhães de Almeida <oficialjoseguilherme@gmail.com>
2026-02-27 13:28:54 -0300

    FIX header.php: adicionando classe nav-item aos elementos

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <header>
     <img id="logo" src="img/logo.png" alt="Logo PETComp">
     <nav class="navbar">
-        <div class="dropdown">
+        <div class="nav-item dropdown">
             <a href="" class="nodropbtn">Início</a>
         </div>
-        <div class="dropdown">
+        <div class="nav-item dropdown">
             <a href="#" class="dropbtn">Conheça o PETComp <span class="material-icons">arrow_drop_down</span></a>
             <div class="dropdown-content">
                 <a href="sobre">Sobre nós</a>
                 <a href="integrantes">Integrantes</a>
             </div>
         </div>
-        <div class="dropdown dropdown_projetos">
+        <div class="nav-item dropdown dropdown_projetos">
             <a href="#" class="dropbtn">Projetos <span class="material-icons">arrow_drop_down</span></a>
             <div class="dropdown-content">
                 <a href="monitoria">Monitoria</a>
@@ -19,7 +19,7 @@
                 <a href="minicurso">Minicursos e oficinas</a>
             </div>
         </div>
-        <div class="dropdown">
+        <div class="nav-item dropdown">
             <a href="#" class="dropbtn">Publicações <span class="material-icons">arrow_drop_down</span></a>
             <div class="dropdown-content">
                 <a href="publicacoes">Trabalhos Publicados</a>
@@ -27,20 +27,20 @@
                 <a href="banners">Banners PETComp</a>
             </div>
         </div>
-        <div class="dropdown">
+        <div class="nav-item dropdown">
             <a href="noticias">Notícias</a>
         </div>
-        <div class="dropdown dropdown_produtos">
+        <div class="nav-item dropdown dropdown_produtos">
             <a href="#" class="dropbtn">Produtos <span class="material-icons">arrow_drop_down</span></a>
             <div class="dropdown-content">
                 <a href="biblioteca">Biblioteca PETComp</a>
                 <a href="podcast">PodCast</a>
             </div>
         </div>
-        <div class="dropdown">
+        <div class="nav-item dropdown">
             <a href="eventos" class="nodropbtn">Eventos</a>
         </div>
-        <div class="dropdown">
+        <div class="nav-item dropdown">
             <a href="registros" class="nodropbtn">Registros</a>
         </div>
     </nav>
