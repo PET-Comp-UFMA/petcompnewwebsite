2407fe68308d8e4e3f2093133367cfd01e28d4c5
Nicolas Caliman <154644444+nicolascaliman@users.noreply.github.com>
2026-03-25 12:03:19 -0300

    corrigindo erros de ortografia

--- sobre.php
+++ sobre.php
@@ -19,7 +19,7 @@ include 'head.php';
 
     <div class="container-body">
         <p> O Programa de Educação Tutorial de Ciência da Computação da UFMA foi criado em 1 de Setembro de 2007, tendo como tutor o Professor Alexandre César Muniz de Oliveira, em 19 de Janeiro de 2016 o Professor Geraldo Braz Junior tornou-se o novo tutor do grupo. E após sua excelente gestão, em 2019 assume o Professor Luis Rivero. O PET Computação já realizou diversas atividades de pesquisa, ensino e extensão. </p>
-        <p> Dentre as atividades desenvolvidas pelo PETComp podemos citar Acalourada (recepção dos calouros), Grupo de Acompanhamento ao Discente (GAD - Monitoria), Fábrica de Software (desenvolvimento de sites, apps como MAMAprev, Peça em casa e SOS MAMA, CientAcalntara), os petianos fazem pesquisas nas mais diversas áreas no laboratórios onde o PETComp é a porta de entrada da maioria, tivemos a produção de diversos minicursos e oficinas, produção e organização de eventos (EACOMP, ERCEMAPI, eJIM 2020 e ENEPET 2020) e o PodComp, o podcast do PETComp. </p>
+        <p> Dentre as atividades desenvolvidas pelo PETComp podemos citar Acalourada (recepção dos calouros), Grupo de Acompanhamento ao Discente (GAD - Monitoria), Fábrica de Software (desenvolvimento de sites, apps como MAMAprev, Peça em casa e SOS MAMA, CientAlcântara), os petianos fazem pesquisas nas mais diversas áreas nos laboratórios onde o PETComp é a porta de entrada da maioria, tivemos a produção de diversos minicursos e oficinas, produção e organização de eventos (EACOMP, ERCEMAPI, eJIM 2020 e ENEPET 2020) e o PodComp, o podcast do PETComp. </p>
     </div>
 
     <div class="gallery">
