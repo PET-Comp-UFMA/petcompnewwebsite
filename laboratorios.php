8e07b7f9c9b7c31df1115cf008dc252ab7a2b8b0
Nicolas Caliman <154644444+nicolascaliman@users.noreply.github.com>
2026-03-27 12:25:24 -0300

    implementação da pág inicial dos labs

---
+++ laboratorios.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php
+$title = "Laboratórios";
+$cssFiles = ['css/laboratorios.css'];
+include 'head.php';
+?>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <div class="container-header">
+        <h2>Laboratórios</h2>
+        <h3>Confira os laboratórios do curso</h3>
+        <h4><a href="index.php">Página Inicial</a></h4>
+        <h4> → Laboratórios</h4>
+    </div>
+
+    <div class="container-body">
+        <p>
+            Os laboratórios de Ciência da Computação e Inteligência Artificial da UFMA (CCET) formam um ecossistema integrado e colaborativo focado em inovação tecnológica e na resolução de problemas complexos. Em vez de atuarem isoladamente, esses espaços se complementam em pesquisas de ponta que envolvem Inteligência Artificial, aprendizado de máquina e visão computacional (com aplicações médicas e em dados geográficos); engenharia de software e interação humano-computador; além de sistemas web avançados, computação pervasiva, mídias interativas, TV digital e jogos. Funcionando como verdadeiros centros de formação, esses laboratórios mantêm as portas abertas para os alunos, que podem ingressar em projetos de pesquisa bastando entrar em contato direto e conversar com os professores responsáveis.
+        </p>
+    </div>
+
+    <div class="grid-container">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-dxt"></div>
+                <img src="assets/images/pag-labs/logo-dxt.png" alt="Logo DEXTERS Lab" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">DEXTERS Lab</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-vip"></div>
+                <img src="assets/images/pag-labs/logo-viplab.png" alt="Logo Vip Lab" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">VIPLAB</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-modal"></div>
+                <img src="assets/images/pag-labs/logo-modal.png" alt="Logo Modal" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">MODAL</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-lint"></div>
+                <img src="assets/svg/pag-labs/logo-lint.svg" alt="Logo Lint" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">LINT²</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-nca"></div>
+                <img src="assets/images/pag-labs/logo-nca.png" alt="Logo NCA" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">NCA</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-lsdi"></div>
+                <img src="assets/images/pag-labs/logo-lsdi.png" alt="Logo LSDi" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">LSDi</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-laws"></div>
+                <img src="assets/images/pag-labs/logo-laws.png" alt="Logo LAWS" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">LAWS</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-inovtec"></div>
+                <img src="assets/images/pag-labs/logo-inovtec.png" alt="Logo INOVTEC" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">INOVTEC</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-lacmor"></div>
+                <img src="assets/images/pag-labs/logo-lacmor.png" alt="Logo LACMOR" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">LACMOR</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-telemidia"></div>
+                <img src="assets/images/pag-labs/logo-telemidia.png" alt="Logo TELEMÍDIA" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">TELEMÍDIA</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="card-bg bg-lidi"></div>
+                <img src="assets/images/pag-labs/logo-lidi.png" alt="Logo LIDI" class="card-logo-img">
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">LIDI</h3>
+                <p class="card-text">Lorem ipsum dolor sit amet</p>
+                <button class="btn-saiba-mais">Saiba mais</button>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
--- router.php
+++ router.php
@@ -1,57 +1,87 @@
-<?php $root = trim(dirname($_SERVER['SCRIPT_NAME']), '/\\'); 
+<?php $root = trim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
 ?>
 <base href="<?= $root ? "/$root/" : '/' ?>">
 
-<?php 
-    //$root = "petcomp-newwebsite";
-    //echo "root: " . $root . "<br>"; // imprime
-    # regex que aceita qualquer string caso seja precedida por '/'
-    # representa os parametros das paginas dinamicas.
-    # pode ser qualquer palavra pois a página que deve validar o parametro.
-    $param = "[/]?(\w*)"; 
+<?php
+//$root = "petcomp-newwebsite";
+//echo "root: " . $root . "<br>"; // imprime
+# regex que aceita qualquer string caso seja precedida por '/'
+# representa os parametros das paginas dinamicas.
+# pode ser qualquer palavra pois a página que deve validar o parametro.
+$param = "[/]?(\w*)";
 
-    $routes = [
-        "$root/" => function () { require("index.php"); },
-        ($root ? "$root/sobre": "sobre") => function () { require("sobre.php"); },
-        ($root ? "$root/monitoria": "monitoria") => function () { require("monitoria.php"); },
-        ($root ? "$root/desenvolvimento": "desenvolvimento") => function () { require("desenvolvimento.php"); },
-        ($root ? "$root/minicurso": "minicurso") => function () { require("minicurso.php"); },
-        ($root ? "$root/revista": "revista") => function () { require("revista.php"); },
-        ($root ? "$root/banners": "banners") => function () { require("banners.php"); },
-        ($root ? "$root/noticias": "noticias") => function () { require("noticias.php"); },
-        ($root ? "$root/publicacoes": "publicacoes") => function () { require("publicacoes.php"); },
-        ($root ? "$root/biblioteca": "biblioteca") => function () { require("biblioteca-petcomp-monitoria.php"); },
-        ($root ? "$root/podcast": "podcast") => function () { require("podcast.php"); },
-        ($root ? "$root/eventos": "eventos") => function () { require("eventos.php"); },
-        ($root ? "$root/registros": "registros") => function () { require("registros.php"); },
-        ($root ? "$root/noticias$param": "noticias$param") => function ($id) {
-            $_GET['id'] = $id;
-            require("noticia.php");
-        },
-        ($root ? "$root/integrantes$param": "integrantes$param") => function ($id) { 
-            $_GET['page'] = $id;
-            require("integrantes.php");
-        }          
-    ];
-
-    function route($path, $routes){
-        foreach ($routes as $pattern => $handler) {
-            if (preg_match("#^$pattern$#", $path, $matches)) {
-                // Remove o primeiro elemento (match completo)
-                array_shift($matches);
-                
-                // Chama o handler com os parâmetros capturados
-                call_user_func_array($handler, $matches);
-                exit; // Importante para não continuar processando
-            }
-        }
-        // Se nenhuma rota corresponder
-        http_response_code(404);
-        echo "Página não encontrada";
+$routes = [
+    "$root/" => function () {
+        require("index.php");
+    },
+    ($root ? "$root/sobre" : "sobre") => function () {
+        require("sobre.php");
+    },
+    ($root ? "$root/laboratorios" : "laboratorios") => function () {
+        require("laboratorios.php");
+    },
+    ($root ? "$root/monitoria" : "monitoria") => function () {
+        require("monitoria.php");
+    },
+    ($root ? "$root/desenvolvimento" : "desenvolvimento") => function () {
+        require("desenvolvimento.php");
+    },
+    ($root ? "$root/minicurso" : "minicurso") => function () {
+        require("minicurso.php");
+    },
+    ($root ? "$root/revista" : "revista") => function () {
+        require("revista.php");
+    },
+    ($root ? "$root/banners" : "banners") => function () {
+        require("banners.php");
+    },
+    ($root ? "$root/noticias" : "noticias") => function () {
+        require("noticias.php");
+    },
+    ($root ? "$root/publicacoes" : "publicacoes") => function () {
+        require("publicacoes.php");
+    },
+    ($root ? "$root/biblioteca" : "biblioteca") => function () {
+        require("biblioteca-petcomp-monitoria.php");
+    },
+    ($root ? "$root/podcast" : "podcast") => function () {
+        require("podcast.php");
+    },
+    ($root ? "$root/eventos" : "eventos") => function () {
+        require("eventos.php");
+    },
+    ($root ? "$root/registros" : "registros") => function () {
+        require("registros.php");
+    },
+    ($root ? "$root/noticias$param" : "noticias$param") => function ($id) {
+        $_GET['id'] = $id;
+        require("noticia.php");
+    },
+    ($root ? "$root/integrantes$param" : "integrantes$param") => function ($id) {
+        $_GET['page'] = $id;
+        require("integrantes.php");
     }
+];
 
-    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH); # extraindo url digitada
-    $path = ltrim($path, '/'); // removendo '/' à esquerda
-    //echo "url: " . $path . "<br>";
-    route($path, $routes); // redirecionando a url pra rota correspondente
+function route($path, $routes)
+{
+    foreach ($routes as $pattern => $handler) {
+        if (preg_match("#^$pattern$#", $path, $matches)) {
+            // Remove o primeiro elemento (match completo)
+            array_shift($matches);
+
+            // Chama o handler com os parâmetros capturados
+            call_user_func_array($handler, $matches);
+            exit; // Importante para não continuar processando
+        }
+    }
+    // Se nenhuma rota corresponder
+    http_response_code(404);
+    echo "Página não encontrada";
+}
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH); # extraindo url digitada
+$path = ltrim($path, '/'); // removendo '/' à esquerda
+//echo "url: " . $path . "<br>";
+route($path, $routes); // redirecionando a url pra rota correspondente
 ?>
