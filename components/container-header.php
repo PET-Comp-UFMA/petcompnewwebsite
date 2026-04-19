bccc39b95bf3f14761b4bd77a617b5ca23712a04
José Guilherme Magalhães de Almeida <oficialjoseguilherme@gmail.com>
2026-04-19 02:45:23 -0300

    UPG components/container-header.php: adequar componente para nova classe Component

--- components/container-header.php
+++ components/container-header.php
@@ -1,9 +1,34 @@
-<div class="container-header">
-    <h2><?= htmlspecialchars($titulo_pagina) ?></h2>
-    <h3><?= htmlspecialchars($descricao) ?></h3>
-    <h4><a href="index.php">Página inicial</a></h4>
-    
-    <?php foreach ($caminho as $subcaminho): ?>
-        <h4> → <?= htmlspecialchars($subcaminho) ?></h4>
-    <?php endforeach; ?>
-</div>
+<?php 
+
+require_once 'core/component.php';
+
+class ContainerHeader extends Component {
+
+    public function __construct(
+        protected string $titulo_pagina,
+        protected string $descricao,
+        protected array $caminho
+    ) {
+        $this->dependencies[] = "../css/monitoria.css";
+    }
+
+    public function render(): String {
+        ob_start();
+        ?>
+
+        <div class="container-header">
+            <h2><?= htmlspecialchars($this->titulo_pagina) ?></h2>
+            <h3><?= htmlspecialchars($this->descricao) ?></h3>
+            <h4><a href="index.php">Página inicial</a></h4>
+            
+            <?php foreach ($this->caminho as $subcaminho): ?>
+                <h4> → <?= htmlspecialchars($subcaminho) ?></h4>
+            <?php endforeach; ?>
+        </div>
+
+        <?php
+        return ob_get_clean();
+    }
+}
+
+?>
