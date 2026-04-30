83499e4efd3f0fe04c36f51156c96748bd96fe34
Daviesdras <140026135+Daviesdras@users.noreply.github.com>
2026-04-30 12:50:49 -0300

    dividindo as monitorias em arquivos diferentes
    dividir pra ser mais escalável

---
+++ monitorias/linguagem-de-programacao-1.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+* Monitoria: Linguagem de programação 1
+* Para adicionar aula: copie um bloco e troque o youtube_id pelo ID do vídeo.
+*/
+
+return[
+        'id'        => 'linguagem-de-programacao-1',
+        'id_json'   => 'lp1',
+        'titulo'    => 'Vídeo Aula de Linguagem de Programação 1',
+        'subtitulo' => 'Monitoria de Linguagem de Programação 1',
+        'descricao' => 'Nas vídeo aulas de Linguagem de Programação 1 você pode assistir gravações de monitorias anteriores, explorando os fundamentos de linguagens de programação, paradigmas, sintaxe, semântica e boas práticas de codificação.',
+        'cor'       => '#f57c00',
+        'icone'     => '💻',
+        'semestres' => ['2025.2', '2025.1', '2024.2'],
+        'aulas'     => [
+            [
+                'titulo'     => 'Monitoria 01 - Paradigmas de Programação',
+                'data'       => '08 de Outubro, 2025',
+                'duracao'    => '1h 50 min',
+                'modulo'     => 'Módulo 1',
+                'semestre'   => '2025.2',
+                'youtube_id' => 'SUBSTITUA_PELO_ID_DO_YOUTUBE',
+            ],
+            [
+                'titulo'     => 'Monitoria 02 - Sintaxe e Semântica',
+                'data'       => '15 de Outubro, 2025',
+                'duracao'    => '2h 10 min',
+                'modulo'     => 'Módulo 1',
+                'semestre'   => '2025.2',
+                'youtube_id' => 'SUBSTITUA_PELO_ID_DO_YOUTUBE',
+            ],
+        ],
+    ];
